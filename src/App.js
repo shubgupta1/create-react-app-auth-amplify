0e7a2ff67ce35a637e68c1f4550d47a1c85c0f35
Shubham Gupta <imshubhamgupta18@gmail.com>
2019-10-28 20:15:19 +1030

    Added a logo for the japan olympics 2020 and a heading to test out if auto deploy works as expected.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import image from "./images/japanolympicslogo.png";
+import "./App.css";
+import { withAuthenticator } from "aws-amplify-react";
+import Amplify, { Auth } from "aws-amplify";
+import aws_exports from "./aws-exports";
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -11,7 +12,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Japan Olympics 2020</h1>
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={image} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
